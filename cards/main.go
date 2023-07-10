19d85121637aa62c1785490a5a6dcb3931a9b83c
friendlyantz <friendlyantz@pm.me>
2023-07-11 09:52:30 +1000

    Array, Slice of Rads and looping

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,30 @@ import "fmt"
 
 func main() {
     // var card string = "Ace of Spades"
-    card := "Ace of Spades" // := is only used when declaring a new variable
-    card = "Five of Diamonds" // = is used to reassign a variable
-    fmt.Println(card)
+    // := is only used when declaring a new variable
+    card_one := "Ace of Spades" 
+
+    // = is used to reassign a variable
+    card_one = "Five of Diamonds"
+
+    fmt.Println(card_one)
+
+    card_two := newCard()
+    fmt.Println(card_two)
+
+    cards := []string{"Joker", newCard(), card_one}
+
+    // append does not modify the existing slice, it returns a new slice
+    cards = append(cards, "Six of Spades") 
+
+    fmt.Println(cards)
+
+    for i, card := range cards {
+        fmt.Println(i, card)
+    }
 }
+
+func newCard() string { // string is the return type
+    return "Eight of Hearts"
+}
+
